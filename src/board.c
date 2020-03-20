29b8a835ea9fa1742cc3a14e6477f5a61c507933
entrepeneur-diogo <61589294+entrepeneur-diogo@users.noreply.github.com>
2020-03-20 15:15:45 +0000

    Update board.c

--- src/board.c
+++ src/board.c
@@ -17,6 +17,16 @@ Board boardInit(int size) {
              // Init board
         }
     }
+ 
+ Board boardPrint(Board){
+  
+     for (int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+             // Print board
+        }
+    }
+ 
+ }
 
     return newBoard;
 }
