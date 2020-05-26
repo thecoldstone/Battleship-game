eaa033ac3a4edeba9a8bc006d8d4ce5885dbf13e
Nikita Zhukov <xzhuko01@vutbr.cz>
2020-05-26 23:19:37 +0100

    getCoordinates

--- v_semaphores/user.c
+++ v_semaphores/user.c
@@ -9,22 +9,23 @@ int main() {
   int RECIEVE = 0;
   int GAME = 1;
 
-  User* user = init_game(1);
+  // User* user = init_game(1);
   
-  printf("Press 'enter' once you are ready\n");
-  getchar();
+  // printf("Press 'enter' once you are ready\n");
+  // getchar();
 
-  system("clear");
+  // system("clear");
 
   /* Game Logic */  
   while(GAME) {
 
     while(SHOOT) {
       // WRITE
-      getStatus(user);
+      // getStatus(user);
       printf("Enter coordinates to shoot: ");
       scanf("%s", BUFFER);
       printf("\n");
+      fflush(0);
       w_shoot(fd_game, BUFFER);
 
       sleep(1);
@@ -47,45 +48,52 @@ int main() {
       sleep(1);
       SHOOT = 0;
       RECIEVE = 1;
-      system("clear");
+      // system("clear");
     }
     
     sleep(1);
 
     while (RECIEVE && GAME) {
-      // READ
-      getStatus(user);
+      // getStatus(user);
       lock(FULL);
       lock(MUTX);
       read(fd_game, BUFFER, sizeof(BUFFER));
-      printf("%s\n", BUFFER);
-      fflush(0);
-      unlock(MUTX);
-      unlock(EMPT);
-
-      sleep(1);
-
+      
       if(strcmp(BUFFER, "exit") == 0) {
         GAME = 0;
         strcpy(BUFFER, "exit");
       } else {
-        strcpy(BUFFER, "MISS");
+
+        char x[4];
+        char y[4];
+
+        if(!getCoordinates(BUFFER, x, y)) {
+          strcpy(BUFFER, "MISS");
+        } 
+
+        #ifdef DEBUG
+        printf("%s\n", BUFFER);
+        #endif
       }
+
+      unlock(MUTX);
+      unlock(EMPT);
+      
+      sleep(1);
 
       // WRITE
       w_shoot(fd_game, BUFFER);
-
       sleep(1);
       SHOOT = 1;
       RECIEVE = 0;
-      system("clear");
+      // system("clear");
     }
 
   }
 
   /* Deallocation */
-  boardDestroy(user->board);
-  userDestroy(user);
+  // boardDestroy(user->board);
+  // userDestroy(user);
   close(fd_game);
   return 0;  
 }
--- v_semaphores/user2.c
+++ v_semaphores/user2.c
@@ -9,51 +9,54 @@ int main() {
   int RECIEVE = 1;
   int GAME = 1;
 
-  User* user = init_game(2);
-  printf("Press 'enter' once you are ready\n");
-  getchar();
+  // User* user = init_game(2);
+  // printf("Press 'enter' once you are ready\n");
+  // getchar();
 
-  system("clear");
+  // system("clear");
 
   /* Game Logic */
   while(GAME) {
 
     while (RECIEVE) {
-      getStatus(user);
+      // getStatus(user);
       lock(FULL);
       lock(MUTX);
       read(fd_game, BUFFER, sizeof(BUFFER));
-      printf("%s\n", BUFFER);
-      fflush(0);
-      unlock(MUTX);
-      unlock(EMPT);
-
-      sleep(1);
-
+      
       if(strcmp(BUFFER, "exit") == 0) {
         GAME = 0;
         strcpy(BUFFER, "exit");
       } else {
-        strcpy(BUFFER, "MISS");
+        char x[4];
+        char y[4];
+
+        if(!getCoordinates(BUFFER, x, y)) {
+          strcpy(BUFFER, "MISS");
+        } 
+        #ifdef DEBUG
+        printf("%s\n", BUFFER);
+        #endif
       }
 
-      lock(EMPT);
-      lock(MUTX);
-      write(fd_game, BUFFER, sizeof(BUFFER));
       unlock(MUTX);
-      unlock(FULL);
+      unlock(EMPT);
+      
+      sleep(1);
+
+      w_shoot(fd_game, BUFFER);
 
       sleep(1);
       SHOOT = 1;
       RECIEVE = 0;
-      system("clear");
+      // system("clear");
     }
    
     sleep(1);
 
     while (SHOOT && GAME) {
       // WRITE
-      getStatus(user);
+      // getStatus(user);
       printf("Enter coordinates to shoot: ");
       scanf("%s", BUFFER);
       printf("\n");
@@ -83,14 +86,14 @@ int main() {
       sleep(1);
       SHOOT = 0;
       RECIEVE = 1;
-      system("clear");
+      // system("clear");
     }
 
   }
 
   /* Deallocation */
-  boardDestroy(user->board);
-  userDestroy(user);
+  // boardDestroy(user->board);
+  // userDestroy(user);
   /* Delete Semaphore */
   if (semctl(SEMID, 0, IPC_RMID) == -1) {
     end_program("semctl");
